45459f8a856a59fe059e4add38a74e49f2d83037
blue <blue@itmo.com>
2026-05-18 00:03:14 +0300

    r6

--- B.java
+++ B.java
@@ -27,4 +27,8 @@ public class B implements A {
     public java.util.Set<Integer> ll() {
         return new java.util.LinkedList<Integer>;
     }
+
+    public byte oo() {
+        return 4;
+    }
 }
