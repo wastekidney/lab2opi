f3362b8e6e69680bb36900929175c29f459e00ac
blue <blue@itmo.com>
2026-05-18 00:00:25 +0300

    r1

---
+++ B.java
@@ -0,0 +1,26 @@
+public class B implements A {
+
+    private int g = 42;
+
+    private long c = 1234;
+
+    public double ad() {
+        return java.lang.Math.sqrt(13);
+    }
+
+    public void bb() {
+        System.out.println(42);
+    }
+
+    public String kk() {
+        return "No";
+    }
+
+    public int[] ii() {
+        return new int[]{0, 1, 2, 3, 4};
+    }
+
+    public int cc() {
+        return 13;
+    }
+}
